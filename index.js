b2b3a6fb0675b702c4b7c178227217ecc3a7961b
Patrick T <enteepee2@gmail.com>
2022-10-03 19:51:26 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 const btnInput = document.getElementById("btn-input")
+const btnInputTab = document.getElementById("btn-input-tab")
+const btnInputDelete = document.getElementById("btn-input-delete")
 const elInput = document.getElementById("el-input")
 const elUl = document.getElementById("el-ul")
 
 let myLeads = []
 
+
+checkLeadsInLocalStorage()
+render(myLeads)
+
+function checkLeadsInLocalStorage() {
+    const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+    leadsFromLocalStorage ? myLeads = leadsFromLocalStorage : console.log( `Local Storage Is Currently Empty.` );
+}
+
 btnInput.addEventListener("click", () => {
-    elInput.value !== `` ? myLeads.push(elInput.value) : ``;
-    elInput.value = ``;
-    renderLeads();
+    if (elInput.value !== ``) {
+        myLeads.push(elInput.value);
+        elInput.value = ``;
+        localStorage.setItem( "myLeads", JSON.stringify(myLeads) );
+    }
+    render(myLeads);
 })
 
-function renderLeads() {
+btnInputTab.addEventListener("click", () => {
+    console.log("Saved tab")
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem( "myLeads", JSON.stringify(myLeads) );
+        render(myLeads)
+    })
+})
+
+btnInputDelete.addEventListener("dblclick", () => {
+    console.log("Deleted all leads")
+    myLeads = []
+    localStorage.clear()
+    render(myLeads)
+})
+
+function render(leads) {
     let listItems = "";
-    for ( let i = 0 ; i < myLeads.length ; i++ ) {
+    for ( let i = 0 ; i < leads.length ; i++ ) {
     listItems += `
         <li> 
-            <a target="_blank" href="http://${myLeads[i]}"> 
-                ${myLeads[i]} 
+            <a target="_blank" href="http://${leads[i]}"> 
+                ${leads[i]} 
             </a>
         </li>
     `;
